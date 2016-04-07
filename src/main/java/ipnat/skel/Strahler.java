6cdf26efa26c33fc6dd09a052f4e5491bc416532
Tiago <tiagoalvespedrosa@gmail.com>
2016-04-07 15:43:37 -0400

    Use viridis to distinguish ourselves from old BAR script

--- src/main/java/ipnat/skel/Strahler.java
+++ src/main/java/ipnat/skel/Strahler.java
@@ -407,8 +407,7 @@ public class Strahler implements PlugIn, DialogListener {
 			if (outIS)
 				imp2.show();
 			ip3.setMinAndMax(0, order);
-			//ColorMaps.applyViridisColorMap(imp3, 0);
-			ColorMaps.applyMagmaColorMap(imp3);
+			ColorMaps.applyViridisColorMap(imp3, 230, false);
 			if (validRootRoi)
 				imp3.setRoi(rootRoi);
 			imp3.show();
