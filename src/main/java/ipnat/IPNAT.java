bcc071e33b0d596423fe7f6c01e783655c119f02
Tiago <tiagoalvespedrosa@gmail.com>
2016-04-07 16:45:09 -0400

    Fix tyop

--- src/main/java/ipnat/IPNAT.java
+++ src/main/java/ipnat/IPNAT.java
@@ -28,7 +28,7 @@ public class IPNAT {
 
 	public static final String VERSION = "0.0.1";
 	public static final String BUILD = "01";
-	public static final String EXTENDED_NAME = "Image Processing for NeuroAnatomy and Tree-like Strucutres";
+	public static final String EXTENDED_NAME = "Image Processing for NeuroAnatomy and Tree-like structures";
 	public static final String ABBREV_NAME = "hIPNAT";
 	public static final String MENU_NAME = "Neurons & Dendriforms";
 	public static final String DOC_URL = "http://imagej.net/hIPNAT";
