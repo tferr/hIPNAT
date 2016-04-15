f8b95963395580a172effee90f8241ac88eb78b2
Tiago <tiagoalvespedrosa@gmail.com>
2016-04-15 10:34:10 -0400

    Release version 1.0.2

--- src/main/java/ipnat/IPNAT.java
+++ src/main/java/ipnat/IPNAT.java
@@ -26,8 +26,8 @@ import ij.IJ;
 
 public class IPNAT {
 
-	public static final String VERSION = "1.0.1";
-	public static final String BUILD = "a";
+	public static final String VERSION = "1.0.2";
+	public static final String BUILD = "";
 	public static final String EXTENDED_NAME = "Image Processing for NeuroAnatomy and Tree-like structures";
 	public static final String ABBREV_NAME = "hIPNAT";
 	public static final String DOC_URL = "http://imagej.net/Neuroanatomy";
