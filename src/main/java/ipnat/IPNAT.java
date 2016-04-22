00182e927d5d42e3fcd782dac48c32afab03828c
Tiago <tiagoalvespedrosa@gmail.com>
2016-04-21 21:27:39 -0400

    Start the next release cycle

--- src/main/java/ipnat/IPNAT.java
+++ src/main/java/ipnat/IPNAT.java
@@ -26,7 +26,7 @@ import ij.IJ;
 
 public class IPNAT {
 
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3a";
 	public static final String BUILD = "";
 	public static final String EXTENDED_NAME = "Image Processing for NeuroAnatomy and Tree-like structures";
 	public static final String ABBREV_NAME = "hIPNAT";
