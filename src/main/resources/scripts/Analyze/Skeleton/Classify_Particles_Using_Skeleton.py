fea88585d6e7d9877456633851601b9f9564e4a5
Tiago <tiagoalvespedrosa@gmail.com>
2016-11-16 14:36:11 -0500

    Rewrite script: - Move bulk to run() - Output statistics to a dedicated table - Color code classified ROIs - Simplify code dramatically - Use IJ2 as much as possible

--- src/main/resources/scripts/Analyze/Skeleton/Classify_Particles_Using_Skeleton.py
+++ src/main/resources/scripts/Analyze/Skeleton/Classify_Particles_Using_Skeleton.py
@@ -6,9 +6,9 @@
 # @String(value=" ", visibility="MESSAGE") spacer
 # @ImagePlus(label="Skeletonizable image", description="Must be 8-bit") impSkel
 # @Double(label="Max. \"snap to\" distance", description="In calibrated units", min=1, max=1000, style="scroll bar", value=5) cutoff_dist
-# @Boolean(label="Measure particles", description="According to Analyze>Set Measurements...", value=false) measure_rois
-# @LogService ls
+# @Boolean(label="Display measurements", value=false) display_measurements
 # @UIService uiService
+# @ImageJ ij
 
 """
     Classify_Particles_Using_Skeleton.py
@@ -20,14 +20,13 @@
     be associated to a skeleton feature if the distance between its centroid and
     the feature is less than or equal to a cuttoff ("snap to") distance.
 
-    :version: 20161020
+    :version: 20161116
     :copyright: 2016 TF
     :url: https://github.com/tferr/hIPNAT
     :license: GPL3, see LICENSE for more details
 """
 
-
-from ij import IJ, Macro
+from ij import IJ
 from ij.measure import Measurements as M, ResultsTable
 from ij.plugin.frame import RoiManager
 from ij.plugin.filter import ParticleAnalyzer as PA
@@ -38,6 +37,7 @@ from sc.fiji.analyzeSkeleton import AnalyzeSkeleton_, SkeletonResult
 
 from net.imagej.table import DefaultGenericTable, GenericColumn
 
+from java.awt import Color
 import math, sys
 
 def addToTable(table, column_header, value):
@@ -104,127 +104,110 @@ def pixel_size(imp):
     cal = imp.getCalibration()
     return min(cal.pixelWidth, cal.pixelHeight)
 
-# Retrieve list of skeleton landmarks (here, end/junction points)
-skel_analyzer = AnalyzeSkeleton_()
-skel_analyzer.setup("", impSkel);
-skel_result = skel_analyzer.run();
-end_points = skel_result.getListOfEndPoints()
-n_junctions = sum(skel_result.getJunctions())
-junction_voxels= skel_result.getListOfJunctionVoxels()
-if not end_points and not junction_voxels:
-    raise RuntimeError(impSkel.getTitle() + " does not seem a valid skeleton.")
 
-# Ensure no data from previous runs exists
-rt = ResultsTable()
-rt.reset()
-rm = RoiManager.getInstance()
-if rm is None:
-    rm = RoiManager()
-rm.reset()
+def run():
 
-# Retrieve centroids from IJ1 ParticleAnalyzer
-rt = ResultsTable()
-impPart.deleteRoi()
-IJ.setThreshold(impPart, threshold_lower, threshold_upper, "No Update")
-pa = PA(PA.ADD_TO_MANAGER, M.CENTROID, rt, size_min, size_max)
-pa.analyze(impPart)
-IJ.resetThreshold(impPart)
-try:
-    cx = rt.getColumn(ResultsTable.X_CENTROID) #X_CENTER_OF_MASS
-    cy = rt.getColumn(ResultsTable.Y_CENTROID) #Y_CENTER_OF_MASS
-    n_particles = len(cx)
-except:
-    raise RuntimeError("Verify parameters: No particles detected.")
+    # Get skeleton features
+    if impSkel.getBitDepth() != 8:
+        error(impSkel.getTitle() + " cannot be processed: It is not 8-bit")
+        return
 
-# Loop through particles' centroids and place each particle in a
-# dedicated list according to its distance to skeleton features
-half_px = min(imp.getCalibration().pixelWidth, imp.getCalibration().pixelHeight) / 2
-n_bp = n_tip = n_shaft = n_hybrid = 0
-j_indices = []
-ep_indices = []
-jep_indices = []
-slab_indices = []
+    skeletonize(impSkel)
+    end_points, junctions, junction_voxels = skeleton_properties(impSkel)
+    if not end_points and not junction_voxels:
+        error(impSkel.getTitle() + " does not seem a valid skeleton.")
+        return
 
-for i in range(n_particles):
+    # For convenience we'll store particles in the ROI Manager
+    rm = getRoiManager()
+    if rm is None:
+        return;
 
-    j_dist = ep_dist = sys.maxint
+    # Retrieve centroids from IJ1 ParticleAnalyzer
+    rt = ResultsTable()
+    impPart.deleteRoi()
+    IJ.setThreshold(impPart, threshold_lower, threshold_upper, "No Update")
+    pa = PA(PA.ADD_TO_MANAGER, M.CENTROID, rt, size_min, size_max)
+    pa.analyze(impPart)
+    IJ.resetThreshold(impPart)
+    try:
+        cx = rt.getColumn(ResultsTable.X_CENTROID)
+        cy = rt.getColumn(ResultsTable.Y_CENTROID)
+        n_particles = len(cx)
+    except:
+        error("Verify parameters: No particles detected.")
+        return
 
-    # Retrieve the distance between this particle and the closest junction voxel
-    for jvoxel in junction_voxels:
-        dist = distance(cx[i], cy[i], jvoxel.x, jvoxel.y)
-        if (dist <= cutoff_dist and dist < j_dist):
-            j_dist = dist
+    # Loop through particles' centroids and categorize each particle according
+    # to its distance to skeleton features. The procedure is simple enough
+    # that we can use traced ROIs directly
+    min_distance = pixel_size(impPart)
+    n_bp = n_tip = n_none = n_both = 0
+    for i in range(n_particles):
 
-    # Retrieve the distance between this particle and the closest end-point
-    for end_point in end_points:
-        dist = distance(cx[i], cy[i], end_point.x, end_point.y)
-        if (dist <= cutoff_dist and dist < ep_dist):
-            ep_dist = dist
+        j_dist = ep_dist = sys.maxint
 
-    # Is particle associated with neither junctions nor end-points?
-    if j_dist > cutoff_dist and ep_dist > cutoff_dist:
-        slab_indices.append(i)
-        n_shaft += 1
-    # Is particle associated with both?
-    elif abs(j_dist - ep_dist) <= half_px:
-        jep_indices.append(i)
-        n_hybrid += 1
-    # Is particle associated with an end-point?
-    elif ep_dist < j_dist:
-        ep_indices.append(i)
-        n_tip += 1
-    # Is particle associated with a junction?
-    elif ep_dist > j_dist:
-        j_indices.append(i)
-        n_bp += 1
-    else:
-        print(">>>Error labeling index", i)
+        # Retrieve the distance between this particle and the closest junction voxel
+        for jvoxel in junction_voxels:
+            dist = distance(cx[i], cy[i], jvoxel.x, jvoxel.y)
+            if (dist <= cutoff_dist and dist < j_dist):
+                j_dist = dist
+
+        # Retrieve the distance between this particle and the closest end-point
+        for end_point in end_points:
+            dist = distance(cx[i], cy[i], end_point.x, end_point.y)
+            if (dist <= cutoff_dist and dist < ep_dist):
+                ep_dist = dist
+
+        roi_id = str(i).zfill(n_particles)
+        roi_name = "Unknown:" + roi_id
+        roi_color = Color.BLUE
+
+        # Is particle associated with neither junctions nor end-points?
+        if j_dist > cutoff_dist and ep_dist > cutoff_dist:
+            roi_name = "NoFeature:" + roi_id
+            n_none += 1
+        # Is particle associated with both?
+        elif abs(j_dist - ep_dist) <= min_distance:
+            roi_name = "JunctionAndTip:" + roi_id
+            roi_color = Color.RED
+            n_both += 1
+        # Is particle associated with an end-point?
+        elif ep_dist < j_dist:
+            roi_name = "Tip:" + roi_id
+            roi_color = Color.GREEN
+            n_tip += 1
+        # Is particle associated with a junction?
+        elif ep_dist > j_dist:
+            roi_name = "Junction:" + roi_id
+            roi_color = Color.MAGENTA
+            n_bp += 1
+
+        roi = rm.getRoi(i)
+        roi.setName(roi_name)
+        roi.setStrokeColor(roi_color)
+
+    # Display result
+    rm.runCommand(impPart, "Show All")
+    rm.runCommand(impSkel, "Show All")
+
+    # Output some measurements
+    if display_measurements:
+        rm.runCommand(impPart, "Deselect")
+        rm.runCommand(impPart, "Measure")
+
+        t = DefaultGenericTable()
+        t.appendRow(impPart.getTitle())
+        addToTable(t, "Junction particles", ratio(n_bp, n_particles))
+        addToTable(t, "Tip particles", ratio(n_tip, n_particles))
+        addToTable(t, "J+T particles", ratio(n_both, n_particles))
+        addToTable(t, "Unc. particles", ratio(n_none, n_particles))
+        addToTable(t, "Junctions w/ particles", ratio(n_bp+n_both, sum(junctions)))
+        addToTable(t, "Tips w/ particles", ratio(n_tip+n_both, len(end_points)))
+        addToTable(t, "Max 'snap-to' dist.", str(cutoff_dist) + impPart.getCalibration().getUnits())
+        addToTable(t, "Threshold range", "%d-%d" % (threshold_lower, threshold_upper))
+        addToTable(t, "Size range", "%d-%d" % (size_min, size_max))
+        ij.ui().show("Skeleton Classified Particles", t)
 
 
-# Now that we've split particles into groups, we should
-# probably set a label image, but the procedure is simple
-# enough that we can use traced ROIs directly
-for i in range(n_particles):
-
-    roi_name = ""
-    roi_id = str(i).zfill(4);
-
-    if i in jep_indices:
-        roi_name = "Hybrid:" + roi_id
-    elif i in j_indices:
-        roi_name = "Junction:" + roi_id
-    elif i in ep_indices:
-        roi_name = "Tip:" + roi_id
-    elif i in slab_indices:
-        roi_name = "Shaft:" + roi_id
-    else:
-        roi_name = "UNKNOWN:" + roi_id
-
-    rm.select(i)
-    rm.runCommand("Rename", roi_name)
-
-rm.runCommand(imp, "Show All")
-rm.runCommand(impSkel, "Show None")
-
-# Output some frequencies. We'll use some convenience
-# functions to log everything to the IJ Log window
-def ratio(n, total):
-    return 0.0 if total is 0 else round(float(n)/total*100, 3)
-def log(*arg):
-    IJ.log("%s" % ''.join(map(str, arg)))
-
-log("\n \n*** Stats for ", imp.getTitle(), " ***")
-log("BP particles: ", n_bp, ", ", ratio(n_bp, n_particles), "%")
-log("Tip particles: ", n_tip, ", ", ratio(n_tip, n_particles), "%")
-log("Shaft particles: ", n_shaft, ", ", ratio(n_shaft, n_particles), "%")
-log("Hybrid particles: ", n_hybrid, ", ", ratio(n_hybrid, n_particles), "%")
-log("Occupied BPs: ", n_bp + n_hybrid, ", ", ratio(n_bp+n_hybrid, n_junctions), "%")
-log("Occupied Tips: ", n_tip + n_hybrid, ", ", ratio(n_tip+n_hybrid, len(end_points)), "%")
-log("\nCutoff distance: ", cutoff_dist, imp.getCalibration().getUnits())
-log("Threshold range: ", threshold_lower, "-", threshold_upper)
-log("Size range: ", size_min, "-", size_max)
-
-# Our job is now done. We'll just measure tagged particles before exiting
-if measure_rois:
-    rm.runCommand(imp, "Deselect")
-    rm.runCommand(imp, "Measure")
+run()
