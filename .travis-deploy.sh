75fbc69e059762d26f51385567bbe83fc29a0b74
Tiago <tiagoalvespedrosa@gmail.com>
2017-05-02 13:24:05 -0400

    Simplifly deploy script and impose update-force-pristine

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -7,28 +7,29 @@ export UPDATE_SITE="Neuroanatomy"
 export URL="http://sites.imagej.net/$UPDATE_SITE/"
 
 # Paths:
-export DEPLOY_PATH="$HOME/deploy"
-export IJ_PATH="$DEPLOY_PATH/Fiji.app"
+export IJ_PATH="$HOME/Fiji.app"
 export IJ_LAUNCHER="$IJ_PATH/ImageJ-linux64"
+export PATH="$IJ_PATH:$PATH"
 
 # Install IJ
 mkdir -p $IJ_PATH/
-cd $DEPLOY_PATH
+cd $HOME/
 wget --no-check-certificate https://downloads.imagej.net/fiji/latest/fiji-linux64.zip
 unzip fiji-linux64.zip
+$IJ_LAUNCHER --update update-force-pristine
 
 # Install artifact
 cd $TRAVIS_BUILD_DIR/
-mvn clean install -Dimagej.app.directory=$IJ_PATH -Ddelete.other.versions=true
+mvn clean package -Dfiji.home=$IJ_PATH
 
 # Deploy if release version
 ARTIFACT=`find $IJ_PATH/plugins/ -type f -print | grep 'hIPNAT_'`
-if [[ $ARTIFACT == *"SNAPSHOT"* ]]; then
+if [[ "$ARTIFACT" == *SNAPSHOT* ]]; then
     echo "SNAPSHOT release: skipping upload to $URL"
 else
-    echo "Uploading %ARTIFACT"
+    echo "Uploading $ARTIFACT"
     echo "Setting $URL credentials"
     $IJ_LAUNCHER --update edit-update-site $UPDATE_SITE $URL "webdav:$UPDATE_SITE:$NEUROANAT_UPLOAD_PASS" .
     echo "Uploading to $URL..."
-    $IJ_LAUNCHER --update upload --update-site $UPDATE_SITE --force-shadow plugins/hIPNAT_.jar
+    $IJ_LAUNCHER --update upload-complete-site --update-site $UPDATE_SITE --force-shadow plugins/hIPNAT_.jar
 fi
