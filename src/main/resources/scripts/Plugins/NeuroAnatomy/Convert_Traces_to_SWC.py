5394ba7e96c7a345c77e28e957bf3b0c34eb2102
Tiago Ferreira <tiago.tferr@gmail.com>
2018-11-28 10:16:55 -0500

    Update converter
    - Adopt new API
    - Do not skip files with uppercase extension
    - Warn when overriding files

--- src/main/resources/scripts/Plugins/NeuroAnatomy/Convert_Traces_to_SWC.py
+++ src/main/resources/scripts/Plugins/NeuroAnatomy/Convert_Traces_to_SWC.py
@@ -1,16 +1,15 @@
-# @String(value="This script converts all .traces files in a directory into SWC. Conversion log will be shown in Console.", visibility="MESSAGE") msg
-# @File(label="Directory of .traces files:", style="directory") input_dir
-# @Boolean(label="Open Console", value=false) open_console
-# @LogService log
-# @StatusService status
-# @UIService ui
+#@String(value="This script converts all .traces files in a directory into SWC. Conversion log is shown in Console.", visibility="MESSAGE") msg
+#@File(label="Directory of .traces files:", style="directory") input_dir
+#@Boolean(label="Open Console", value=false) open_console
+#@LogService log
+#@StatusService status
+#@UIService ui
 
 '''
 file:       Convert_Traces_to_SWC.py
-author:     Mark Longair / Tiago Ferreira
-version:    20170203
-info:       Converts all .traces files in a directory into SWC.
-            Based on https://gist.github.com/mhl/888051
+author:     Tiago Ferreira
+version:    20181128
+info:       Converts all .traces files in a directory into SWC
 '''
 
 import os, re
@@ -25,26 +24,22 @@ def run():
     conversion_counter = 0
     d = str(input_dir)
     log.info('Processing %s...' % d)
-    for e in os.listdir(d):
-        if os.path.basename(e).startswith('.'):
+    for f in os.listdir(d):
+        if os.path.basename(f).startswith('.'):
             continue
-        if not e.endswith('.traces'):
-            log.info('Skipping ' + e + '...')
+        if re.search('.*-exported-\d{3}.swc', f):
+            log.warn("'%s' already exists" % f)
+        if not f.lower().endswith('.traces'):
+            log.info('Skipping %s...' % f)
             continue
-        traces_filename = os.path.join(d, e)
-        swc_filename_prefix = re.sub(r'\.traces', '-exported', traces_filename)
-        log.info('Converting %s to %s-*.swc' % (traces_filename, swc_filename_prefix))
-        pafm = PathAndFillManager()
-        try:
-            pafm.loadGuessingType(traces_filename)
-            if pafm.checkOKToWriteAllAsSWC(swc_filename_prefix):
-                pafm.exportAllAsSWC(swc_filename_prefix)
-                conversion_counter += 1
-            else:
-                raise IOError
-        except IOError:
-            log.error('Could not convert ' + traces_filename)
+        file_path = os.path.join(d, f)
+        swc_filename_prefix = re.sub(r'\.traces', '-exported', file_path)
+        log.info('Converting %s to %s-*.swc' % (file_path, swc_filename_prefix))
+        pafm = PathAndFillManager.createFromFile(file_path)
+        if pafm.exportAllPathsAsSWC(swc_filename_prefix):
+            conversion_counter += 1
+        else:
+            log.error('Could not convert %s' % file_path)
     log.info(str(conversion_counter) + ' file(s) successfully converted')
 
-if __name__ == '__main__':
-    run()
+run()
